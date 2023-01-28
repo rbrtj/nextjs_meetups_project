a1b986fcd2c8ebfcfd19e1916b32c982da41978a
rbrtj <robertjaszczurek@icloud.com>
2023-01-28 17:18:29 +0100

    Added global layout

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ff5ca17c57842d03"; }
+/******/ 		__webpack_require__.h = function() { return "48180398407d3466"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
